9dfae7aa1c54c1ff69318e6498eed327a4c58558
ryan liang <ryan@ryans-MacBook.local>
2017-05-16 23:18:06 -0700

    features completedgit add com

--- com/ryanliang/inventorygui/InventoryView.java
+++ com/ryanliang/inventorygui/InventoryView.java
@@ -37,6 +37,9 @@ public class InventoryView extends JFrame implements Viewable{
 	private final Controllable controller;
 	private Modellable model;
 	
+	private Media [] searchResult;
+	private int resultCounter = 0;
+	
 	private static String itemID = "0";
 	
 	private ItemDialog itemDialog = null;
@@ -73,17 +76,11 @@ public class InventoryView extends JFrame implements Viewable{
 	private static final JButton lastToolBarButton = new JButton("Last"); 
 	
 	private static final JToolBar toolBar = new JToolBar();
-	private JTable table = null;
-	private JScrollPane scrollPane = null;
 	
 	private static final JPanel westPanel = new JPanel();
 	private static final JPanel statusPanel = new JPanel();
 	private static final JLabel totalContactsLabel = new JLabel("Total Contacts: ");
 	private JLabel totalContactsStatus = new JLabel("");
-	private int totalContact = 0;
-	
-	private int tableRowNum = 0;
-	private boolean tableRowSelected = false; 
 	
 	private JLabel itemDetails = new JLabel("");
 	
@@ -153,8 +150,40 @@ public class InventoryView extends JFrame implements Viewable{
 		findToolBarButton.addActionListener(event -> searchItem());
 		deleteToolBarButton.addActionListener(event -> deleteItem());
 		editToolBarButton.addActionListener(event -> editItem());
+		nextToolBarButton.addActionListener(event -> nextItem());
+		previousToolBarButton.addActionListener(event -> previousItem());
+		firstToolBarButton.addActionListener(event -> firstItem());
+		lastToolBarButton.addActionListener(event -> lastItem());
       
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private void lastItem() {
+		if (searchResult.length > 1){
+			resultCounter = searchResult.length - 1;
+			displayResult(searchResult[resultCounter]);		
+		}	
+	}
+
+	private void firstItem() {
+		if (searchResult.length > 1){
+			resultCounter = 0;
+			displayResult(searchResult[resultCounter]);		
+		}	
+	}
+
+	private void nextItem() {
+		if (searchResult.length > 1 && (resultCounter < (searchResult.length-1))){
+			resultCounter++;
+			displayResult(searchResult[resultCounter]);		
+		}	
+	}
+
+	private void previousItem() {
+		if (searchResult.length > 1 && (resultCounter > 0)){
+			resultCounter--;
+			displayResult(searchResult[resultCounter]);		
+		}	
 	}
 
 	private void searchItem() {
@@ -230,105 +259,23 @@ public class InventoryView extends JFrame implements Viewable{
 	@Override
 	public void update(UpdateType ut) {
 		if (ut == UpdateType.SEARCH_RESULT){
-			Media [] result = model.getSearchResult();
+			searchResult = model.getSearchResult();
 			
-			if (result.length < 1)
+			if (searchResult.length < 1)
 				JOptionPane.showMessageDialog(null, "Item does not exist", "alert", JOptionPane.ERROR_MESSAGE); 
-			else{
-				for (Media mm : result){
-					
-					//****More code is needed for multiple search results (items)!!	****
-					
-					if (mm instanceof CD){						
-						itemDetails.setText("<html>" + "<h3><font color=blue>Item ID</font></h3>" + mm.getID() + "<br>" 
-								+ "<h3><font color=blue>Quantity</font></h3>" + model.getItemQuantity(mm.getID()) + "<br>"
-								+ "<h3><font color=blue>Title</font></h3>" + mm.getTitle() + "<br>" 
-								+ "<h3><font color=blue>Artist(s)</font></h3>" + ((CD)mm).getArtist() + "<br>"
-								+ "<h3><font color=blue>Genre</font></h3>" + mm.getGenre() + "<br>" 
-								+ "<h3><font color=blue>Description</font></h3>" + mm.getDescription() + "<br>" +  "</html>");
-					}
-					else if (mm instanceof DVD){						
-						itemDetails.setText("<html>" + "<h3><font color=blue>Item ID</font></h3>" + mm.getID() + "<br>" 
-								+ "<h3><font color=blue>Quantity</font></h3>" + model.getItemQuantity(mm.getID()) + "<br>"
-								+ "<h3><font color=blue>Title</font></h3>" + mm.getTitle() + "<br>" 
-								+ "<h3><font color=blue>Cast(s)</font></h3>" + ((DVD)mm).getCast() + "<br>"
-								+ "<h3><font color=blue>Genre</font></h3>" + mm.getGenre() + "<br>" 
-								+ "<h3><font color=blue>Description</font></h3>" + mm.getDescription() + "<br>" +  "</html>");
-					}
-					else if (mm instanceof Book){						
-						itemDetails.setText("<html>" + "<h3><font color=blue>Item ID</font></h3>" + mm.getID() + "<br>" 
-								+ "<h3><font color=blue>Quantity</font></h3>" + model.getItemQuantity(mm.getID()) + "<br>"
-								+ "<h3><font color=blue>Title</font></h3>" + mm.getTitle() + "<br>" 
-								+ "<h3><font color=blue>Author(s)</font></h3>" + ((Book)mm).getAuthor() + "<br>"
-								+ "<h3><font color=blue>ISBN</font></h3>" + ((Book)mm).getISBN() + "<br>"
-								+ "<h3><font color=blue>Genre</font></h3>" + mm.getGenre() + "<br>" 
-								+ "<h3><font color=blue>Description</font></h3>" + mm.getDescription() + "<br>" +  "</html>");
-					}
-					validate();
-				}
-			}
+			else {
+				//reset counter
+				resultCounter = 0;
+				displayResult(searchResult[0]);		
+			}			
 		}
 		else if (ut == UpdateType.EDIT){
 			Media [] result = model.getSearchResult();
 			
 			if (result.length < 1)
 				JOptionPane.showMessageDialog(null, "Item does not exist", "alert", JOptionPane.ERROR_MESSAGE); 
-			else{
-				for (Media mm : result){
-
-					if (mm instanceof CD){	
-						
-						openItemDialog(mm, model.getItemQuantity(mm.getID()));		
-						if (itemDialog.getDone() == true){	
-							//may need to check null value.  will analyze later.
-							CD item = (CD) itemDialog.getItem();
-							
-							String ID = item.getID();
-							String title = item.getTitle();
-							String quantity = itemDialog.getQuantity();
-							String genre = item.getGenre();
-							String description = item.getDescription();
-							String artist = item.getArtist();
-
-							controller.editItem(new CD(ID, title, description , genre, artist), quantity); 
-						}
-					}
-					else if (mm instanceof DVD){	
-						
-						openItemDialog(mm, model.getItemQuantity(mm.getID()));		
-						if (itemDialog.getDone() == true){	
-							DVD item = (DVD) itemDialog.getItem();
-
-							String ID = item.getID();
-							String title = item.getTitle();
-							String quantity = itemDialog.getQuantity();
-							String genre = item.getGenre();
-							String description = item.getDescription();
-							String cast = item.getCast();
-
-							controller.editItem(new DVD(ID, title, description , genre, cast), quantity); 
-						}
-					}
-					else if (mm instanceof Book){	
-						
-						openItemDialog(mm, model.getItemQuantity(mm.getID()));		
-						if (itemDialog.getDone() == true){	
-							Book item = (Book) itemDialog.getItem();
-
-							String ID = item.getID();
-							String title = item.getTitle();
-							String quantity = itemDialog.getQuantity();
-							String genre = item.getGenre();
-							String description = item.getDescription();
-							String author = item.getAuthor();
-							String ISBN = item.getISBN();
-
-							controller.editItem(new Book(ID, title, description , genre, author, ISBN), quantity); 
-						}
-					}
-				
-				}
-			}
+			else
+				editResult(result[0]);			
 		}
 		else if (ut == UpdateType.ID){
 			itemID = model.getID();
@@ -336,5 +283,86 @@ public class InventoryView extends JFrame implements Viewable{
 
 	}
 
+	private void editResult(Media mm) {
+		if (mm instanceof CD){	
+
+			openItemDialog(mm, model.getItemQuantity(mm.getID()));		
+			if (itemDialog.getDone() == true){	
+				//may need to check null value.  will analyze later.
+				CD item = (CD) itemDialog.getItem();
+
+				String ID = item.getID();
+				String title = item.getTitle();
+				String quantity = itemDialog.getQuantity();
+				String genre = item.getGenre();
+				String description = item.getDescription();
+				String artist = item.getArtist();
+
+				controller.editItem(new CD(ID, title, description , genre, artist), quantity); 
+			}
+		}
+		else if (mm instanceof DVD){	
+
+			openItemDialog(mm, model.getItemQuantity(mm.getID()));		
+			if (itemDialog.getDone() == true){	
+				DVD item = (DVD) itemDialog.getItem();
+
+				String ID = item.getID();
+				String title = item.getTitle();
+				String quantity = itemDialog.getQuantity();
+				String genre = item.getGenre();
+				String description = item.getDescription();
+				String cast = item.getCast();
+
+				controller.editItem(new DVD(ID, title, description , genre, cast), quantity); 
+			}
+		}
+		else if (mm instanceof Book){	
+
+			openItemDialog(mm, model.getItemQuantity(mm.getID()));		
+			if (itemDialog.getDone() == true){	
+				Book item = (Book) itemDialog.getItem();
+
+				String ID = item.getID();
+				String title = item.getTitle();
+				String quantity = itemDialog.getQuantity();
+				String genre = item.getGenre();
+				String description = item.getDescription();
+				String author = item.getAuthor();
+				String ISBN = item.getISBN();
+
+				controller.editItem(new Book(ID, title, description , genre, author, ISBN), quantity); 
+			}
+		}
+	}
+
+	private void displayResult(Media mm) {
+		if (mm instanceof CD){						
+			itemDetails.setText("<html>" + "<h3><font color=blue>Item ID</font></h3>" + mm.getID() + "<br>" 
+					+ "<h3><font color=blue>Quantity</font></h3>" + model.getItemQuantity(mm.getID()) + "<br>"
+					+ "<h3><font color=blue>Title</font></h3>" + mm.getTitle() + "<br>" 
+					+ "<h3><font color=blue>Artist(s)</font></h3>" + ((CD)mm).getArtist() + "<br>"
+					+ "<h3><font color=blue>Genre</font></h3>" + mm.getGenre() + "<br>" 
+					+ "<h3><font color=blue>Description</font></h3>" + mm.getDescription() + "<br>" +  "</html>");
+		}
+		else if (mm instanceof DVD){						
+			itemDetails.setText("<html>" + "<h3><font color=blue>Item ID</font></h3>" + mm.getID() + "<br>" 
+					+ "<h3><font color=blue>Quantity</font></h3>" + model.getItemQuantity(mm.getID()) + "<br>"
+					+ "<h3><font color=blue>Title</font></h3>" + mm.getTitle() + "<br>" 
+					+ "<h3><font color=blue>Cast(s)</font></h3>" + ((DVD)mm).getCast() + "<br>"
+					+ "<h3><font color=blue>Genre</font></h3>" + mm.getGenre() + "<br>" 
+					+ "<h3><font color=blue>Description</font></h3>" + mm.getDescription() + "<br>" +  "</html>");
+		}
+		else if (mm instanceof Book){						
+			itemDetails.setText("<html>" + "<h3><font color=blue>Item ID</font></h3>" + mm.getID() + "<br>" 
+					+ "<h3><font color=blue>Quantity</font></h3>" + model.getItemQuantity(mm.getID()) + "<br>"
+					+ "<h3><font color=blue>Title</font></h3>" + mm.getTitle() + "<br>" 
+					+ "<h3><font color=blue>Author(s)</font></h3>" + ((Book)mm).getAuthor() + "<br>"
+					+ "<h3><font color=blue>ISBN</font></h3>" + ((Book)mm).getISBN() + "<br>"
+					+ "<h3><font color=blue>Genre</font></h3>" + mm.getGenre() + "<br>" 
+					+ "<h3><font color=blue>Description</font></h3>" + mm.getDescription() + "<br>" +  "</html>");
+		}
+		validate();
+	}
 
 }
